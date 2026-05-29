cfd8b4dee47164235340ee9eb17012994eec1969
Goutham Kulall <gouthamkulall615@gmail.com>
2026-05-29 21:26:28 +0530

    understood the insertion sort technique

--- Sorting Technique/BubbleSort.cpp
+++ Sorting Technique/BubbleSort.cpp
@@ -9,14 +9,23 @@ int main()
 
     for (int i = 0; i < n - 1; i++)
     {
+
+        bool swapped = false;
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
+
+        if (swapped == false)
+        {
+            break;
+        }
     }
+
     cout << "sorted arr is :" << endl;
 
     for (int i = 0; i < n; i++)
---
+++ Sorting Technique/InsertionSort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 1, 7, 4, 8, 2, 11};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array is:" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
