a155a882df322b4eb75fe1cdd9e0a650f1ade18c
Goutham Kulall <gouthamkulall615@gmail.com>
2026-05-30 12:30:38 +0530

    solved the reverse an array

--- Arrays/reverseAnArray.cpp
+++ Arrays/reverseAnArray.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverse(int arr[], int n)
+int main()
 {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
     int start = 0;
     int end = n - 1;
     while (start <= end)
     {
         swap(arr[start], arr[end]);
-        start++;
-        end--;
     }
-}
-void printArray(int arr[], int n)
-{
+    cout << "the reversed array is" << endl;
+
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-    cout << endl;
-}
-
-int main()
-{
-    int arr[5] = {1, 2, 3, 4, 5};
-    reverse(arr, 5);
-    printArray(arr, 5);
     return 0;
 }
