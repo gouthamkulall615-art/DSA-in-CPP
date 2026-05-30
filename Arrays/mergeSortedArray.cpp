14657cf03b4f1a7cad1362212431f80747ab3946
Goutham Kulall <gouthamkulall615@gmail.com>
2026-05-30 12:48:01 +0530

    solved the merge two sorted arrary

--- Arrays/mergeSortedArray.cpp
+++ Arrays/mergeSortedArray.cpp
@@ -10,15 +10,32 @@ void merge(int arr1[], int n, int arr2[], int m, int arr3[])
     {
         if (arr1[i] < arr2[j])
         {
-            arr3[k] = arr1[i];
-            k++;
-            i++;
+            arr3[k++] = arr1[i++];
         }
+        else
+        {
+            arr3[k++] = arr2[j++];
+        }
+    }
+    while (i < n)
+    {
+        arr3[k++] = arr1[i++];
+    }
+
+    while (j < m)
+    {
+        arr3[k++] = arr2[j++];
     }
 }
 
 void print(int arr3[], int p)
 {
+
+    cout << "the merged array is" << endl;
+    for (int i = 0; i < p; i++)
+    {
+        cout << arr3[i] << " ";
+    }
 }
 
 int main()
