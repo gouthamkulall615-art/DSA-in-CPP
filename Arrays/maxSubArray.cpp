a0ce9b0d41bf4794ca9062a27757be31683a8a2f
Goutham Kulall <gouthamkulall615@gmail.com>
2026-06-10 20:54:26 +0530

    added how to get the subarray which gives the maximum sum

--- Arrays/maxSubArray.cpp
+++ Arrays/maxSubArray.cpp
@@ -4,17 +4,36 @@ int maxSubArray(vector<int> &nums)
 {
     int sum = 0;
     int maxi = INT_MIN;
+    int start = 0;
+    int ansStart = 0;
+    int ansEnd = 0;
 
-        for (int i = 0; i < nums.size(); i++)
+    for (int i = 0; i < nums.size(); i++)
     {
+
+        if (sum == 0)
+        {
+            start = i;
+        }
+
         sum += nums[i];
         if (sum > maxi)
+        {
             maxi = sum;
+            ansStart = start;
+            ansEnd = i;
+        }
 
         if (sum < 0)
         {
             sum = 0;
         }
+    }
+
+    cout << "the subarray is" << endl;
+    for (int i = ansStart; i <= ansEnd; i++)
+    {
+        cout << nums[i] << " ";
     }
     return maxi;
 }
