6e62adf4a7a21dd725028fe55ea1a0bf2f56cb77
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-09 18:34:11 +0530

    solved the container with most water using the two pointer approch

--- Arrays/containerWithMostWater.cpp
+++ Arrays/containerWithMostWater.cpp
@@ -1,3 +1,37 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int containerWithMostWater(vector<int> &nums)
+// {
+//     int maxWater = 0;
+//     int n = nums.size();
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             int width = j - i;
+//             int height = min(nums[i], nums[j]);
+
+//             int area = width * height;
+//             maxWater = max(maxWater, area);
+//         }
+//     }
+//     return maxWater;
+// }
+// int main()
+// {
+
+//     vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+//     int result = containerWithMostWater(height);
+
+//     cout << " the answer is " << result;
+//     return 0;
+// }
+
+// optimal solution
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,16 +40,17 @@ int containerWithMostWater(vector<int> &nums)
     int maxWater = 0;
     int n = nums.size();
 
-    for (int i = 0; i < n; i++)
+    int left = 0;
+    int right = n - 1;
+    while (left < right)
     {
-        for (int j = i + 1; j < n; j++)
-        {
-            int width = j - i;
-            int height = min(nums[i], nums[j]);
+        int width = right - left;
+        int height = min(nums[left], nums[right]);
 
-            int area = width * height;
-            maxWater = max(maxWater, area);
-        }
+        int area = width * height;
+        maxWater = max(maxWater, area);
+
+        nums[left] < nums[right] ? left++ : right--;
     }
     return maxWater;
 }
