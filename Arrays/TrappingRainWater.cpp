219a65d976dec3075e5461943e7d30bc9766e990
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-09 20:36:50 +0530

    solved the trapping rain water using the two pointer approch

--- Arrays/TrappingRainWater.cpp
+++ Arrays/TrappingRainWater.cpp
@@ -1,3 +1,5 @@
+// 
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,28 +8,25 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
+
         int n = height.size();
-        if (n == 0) return 0;
 
         int ans = 0;
-        vector<int> lmax(n), rmax(n);
+        int l = 0, r = n - 1;
 
-        lmax[0] = height[0];
-        rmax[n - 1] = height[n - 1];
+        int lmax = 0, rmax = 0;
 
-        // Fill left max array
-        for (int i = 1; i < n; i++) {
-            lmax[i] = max(lmax[i - 1], height[i]);
-        }
+        while (l < r) {
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
 
-        // Fill right max array
-        for (int i = n - 2; i >= 0; i--) {
-            rmax[i] = max(rmax[i + 1], height[i]);
-        }
-
-        // Calculate trapped water
-        for (int i = 0; i < n; i++) {
-            ans += min(lmax[i], rmax[i]) - height[i];
+            if (lmax < rmax) {
+                ans += (lmax - height[l]);
+                l++;
+            } else {
+                ans += (rmax - height[r]);
+                r--;
+            }
         }
 
         return ans;
