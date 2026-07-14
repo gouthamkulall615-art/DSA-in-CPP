ae73969730a0225a2a9d1a7ea3658b6126fbf4ac
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-14 20:50:51 +0530

    done with the better approch using the hashset

--- Arrays/4sum.cpp
+++ Arrays/4sum.cpp
@@ -1,3 +1,71 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+//     int n = nums.size();
+//     set<vector<int>> st;
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             for (int k = j + 1; k < n; k++) {
+//                 for (int l = k + 1; l < n; l++) {
+
+//                     long long sum = (long long)nums[i] + nums[j];
+//                     sum += nums[k];
+//                     sum += nums[l];
+
+//                     if (sum == target) {
+//                         vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+//                         sort(temp.begin(), temp.end());
+//                         st.insert(temp);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+// int main() {
+
+//     int n;
+//     cout << "Enter the size of the array: ";
+//     cin >> n;
+
+//     vector<int> nums(n);
+
+//     cout << "Enter the elements of the array:\n";
+//     for (int i = 0; i < n; i++) {
+//         cin >> nums[i];
+//     }
+
+//     int target;
+//     cout << "Enter the target sum: ";
+//     cin >> target;
+
+//     vector<vector<int>> ans = fourSum(nums, target);
+
+//     if (ans.empty()) {
+//         cout << "No quadruplets found." << endl;
+//     } else {
+//         cout << "Quadruplets are:\n";
+//         for (auto &vec : ans) {
+//             for (int x : vec) {
+//                 cout << x << " ";
+//             }
+//             cout << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+//better solution
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,20 +75,24 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     set<vector<int>> st;
 
     for (int i = 0; i < n; i++) {
+
         for (int j = i + 1; j < n; j++) {
+
+            set<long long> hashset;
+
             for (int k = j + 1; k < n; k++) {
-                for (int l = k + 1; l < n; l++) {
 
-                    long long sum = (long long)nums[i] + nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
+                long long sum = (long long)nums[i] + nums[j] + nums[k];
+                long long fourth = (long long)target - sum;
 
-                    if (sum == target) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
+                if (hashset.find(fourth) != hashset.end()) {
+
+                    vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
                 }
+
+                hashset.insert(nums[k]);
             }
         }
     }
@@ -49,7 +121,7 @@ int main() {
     vector<vector<int>> ans = fourSum(nums, target);
 
     if (ans.empty()) {
-        cout << "No quadruplets found." << endl;
+        cout << "No quadruplets found.\n";
     } else {
         cout << "Quadruplets are:\n";
         for (auto &vec : ans) {
