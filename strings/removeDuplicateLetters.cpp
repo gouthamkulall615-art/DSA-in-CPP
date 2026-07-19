fbdfb622f02136e52dace8df448af07a2023df93
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-19 17:06:00 +0530

    solve the remove duplicates in the string using the stack

---
+++ strings/removeDuplicateLetters.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeDuplicateLetters(string s)
+{
+    vector<int> last(26);
+    for (int i = 0; i < s.size(); i++)
+    {
+        last[s[i] - 'a'] = i;
+    }
+    stack<char> st;
+    vector<bool> inStack(26, false);
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        char ch = s[i];
+
+        if (inStack[ch - 'a'])
+            continue;
+
+        while (!st.empty() && st.top() > ch && last[st.top() - 'a'] > i)
+        {
+            inStack[st.top() - 'a'] = false;
+            st.pop();
+        }
+
+        st.push(ch);
+        inStack[ch - 'a'] = true;
+    }
+    string ans = "";
+    while (!st.empty())
+    {
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+int main()
+
+{
+    string s = "cbacdcbc";
+    cout << "Answer is : " << removeDuplicateLetters(s) << endl;
+
+    return 0;
+}
