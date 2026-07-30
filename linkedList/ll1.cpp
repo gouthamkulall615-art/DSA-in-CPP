0b9a367d3869138e98fd2a67c030c5d09b62222b
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-30 20:32:43 +0530

    converted the array to LL

--- linkedList/ll1.cpp
+++ linkedList/ll1.cpp
@@ -22,11 +22,25 @@ public:
     }
 };
 
+Node* convertArrToLL(vector<int>& arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
 int main()
 {
     vector<int> arr = {2, 5, 8, 7};
-    Node *y = new Node(arr[0]);
-    cout << y->data;
+    // Node *y = new Node(arr[0]);
+    // cout << y->data;
+
+    Node* head =convertArrToLL(arr);
+    cout<<head->data;
 
     return 0;
 }
