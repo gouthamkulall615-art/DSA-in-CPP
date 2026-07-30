c445e7206c59a907ed50bb797e382d2a8cd5a1dd
Goutham Kulall <gouthamkulall615@gmail.com>
2026-07-30 19:53:42 +0530

    simple program for linked list

---
+++ linkedList/ll1.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {2, 5, 8, 7};
+    Node *y = new Node(arr[0]);
+    cout << y->data;
+
+    return 0;
+}
+
+/*
+
+
+Stack                          Heap
+
++--------+                  Address 1000
+| y      |                  +------------------+
+| 1000 --|----------------->| data = 2         |
++--------+                  | next = nullptr   |
+                            +------------------+
+
+y stores only the address.
+
+y->data
+
+↓
+
+Go to address 1000
+
+↓
+
+Read data
+
+↓
+
+2
+
+*/
