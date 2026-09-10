f4d39f073b29b980a13e716560fd5a4091d41868
Goutham Kulall <gouthamkulall615@gmail.com>
2026-09-10 18:04:41 +0530

    implemented the queue using the array

--- Stack&Queue/Queue/implementQueueUsingArray.cpp
+++ Stack&Queue/Queue/implementQueueUsingArray.cpp
@@ -1,7 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 5
+int queueArr[MAX];
+int front = -1;
+int rear = -1;
 
-int main() {
-    
+void enqueue(int val)
+{
+    if (rear == MAX - 1)
+    {
+        cout << "queue is full\n";
+        return;
+    }
+    else
+    {
+        if (front == -1)
+        {
+            front = 0;
+        }
+        rear++;
+        queueArr[rear] = val;
+    }
+}
+
+void dequeue()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "queue is empty\n";
+        return;
+    }
+    else
+    {
+        cout << "deleted " << queueArr[front] << endl;
+        front++;
+    }
+}
+
+void display()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "queue is empty\n";
+        return;
+    }
+    else
+    {
+        for (int i = front; i <= rear; i++)
+        {
+            cout << queueArr[i] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    cout << "Queue: ";
+    display();
+
+    dequeue();
+
+    cout << "After dequeue: ";
+    display();
+
     return 0;
 }
