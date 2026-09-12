933b6310cb224bae3faf8136d150a5126598eb78
Goutham Kulall <gouthamkulall615@gmail.com>
2026-09-12 13:35:59 +0530

    solved the infix to postfix expression using the stack

---
+++ Stack&Queue/Stack/infixToPostrix.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int precedence(char c)
+{
+    if (c == '^')
+        return 3;
+    if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main()
+{
+    string infix, postfix = "";
+    stack<char> s;
+    cout << "enter the infix expression:";
+    cin >> infix;
+
+    for (char c : infix)
+    {
+        if (isalnum(c))
+        {
+            postfix += c;
+        }
+        else if (c == '(')
+        {
+            s.push(c);
+        }
+        else if (c == ')')
+        {
+            while (!s.empty() && s.top() != '(')
+            {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        else
+        {
+            while (!s.empty() && precedence(s.top()) >= precedence(c))
+            {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+
+    while (!s.empty())
+    {
+        postfix += s.top();
+        s.pop();
+    }
+    cout << "postfix expression" << postfix;
+    return 0;
+}
