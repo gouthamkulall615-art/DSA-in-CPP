f0ee2807fb152c1cbdf668f90057730cb95a3a21
Goutham Kulall <gouthamkulall615@gmail.com>
2026-09-12 14:41:57 +0530

    solved the postfix to prefix expression

---
+++ Stack&Queue/Stack/postfixToprefix.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string postfix, a, b;
+    stack<string> s;
+
+    cout << "enter postfix expression";
+    cin >> postfix;
+
+    for (char c : postfix)
+    {
+        if (isalnum(c))
+        {
+            s.push(string(1, c));
+        }
+        else
+        {
+            b = s.top();
+            s.pop();
+
+            a = s.top();
+            s.pop();
+
+            s.push(string(1, c) + a + b);
+        }
+    }
+    cout << "prefix expression " << s.top();
+
+    return 0;
+}
